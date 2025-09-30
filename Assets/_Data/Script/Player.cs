a6cdd54a46fe1f7be9bb6f781ab4f1f802dc57ff
votantai3070 <votantai.07092002@gmail.com>
2025-10-01 02:50:00 +0700

    Add Aim For Player

--- Assets/_Data/Script/Player.cs
+++ Assets/_Data/Script/Player.cs
@@ -5,11 +5,19 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
 
     [Header("Movement Data")]
-    public float moveSpeed;
-    public float rotationSpeed;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotationSpeed;
 
-    public float verticalInput;
-    public float horizontalInput;
+    float verticalInput;
+    float horizontalInput;
+
+    [Header("Tower Data")]
+    [SerializeField] Transform towerTransform;
+    [SerializeField] float towerRotationSpeed;
+
+    [Header("Aim Data")]
+    [SerializeField] LayerMask whatIsAimMask;
+    [SerializeField] Transform aimTransform;
 
     private void Start()
     {
@@ -17,6 +25,23 @@ public class Player : MonoBehaviour
     }
 
     void Update()
+    {
+        UpdateAim();
+        CheckInputs();
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Move and rotate player
+        ApplyMovement();
+
+        ApplyBodyRotation();
+
+        // Rotate tower
+        ApplyTowerRotation();
+    }
+    private void CheckInputs()
     {
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
@@ -27,11 +52,35 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void ApplyTowerRotation()
+    {
+        Vector3 direction = aimTransform.position - towerTransform.position;
+        direction.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        towerTransform.rotation = Quaternion.RotateTowards(towerTransform.rotation, targetRotation, towerRotationSpeed);
+    }
+
+    private void ApplyBodyRotation()
+    {
+        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    }
+
+    private void ApplyMovement()
     {
         Vector3 movement = moveSpeed * verticalInput * transform.forward;
         rb.linearVelocity = movement;
+    }
 
-        transform.Rotate(0, horizontalInput * rotationSpeed, 0);
+    private void UpdateAim()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, whatIsAimMask))
+        {
+            float fixedY = aimTransform.position.y;
+            aimTransform.position = new Vector3(hit.point.x, fixedY, hit.point.z);
+        }
     }
 }
